add059febba28d1a6e60c4197e34ed6a01c22dd8
Keita Yamaguchi <keita.yamaguchi@gmail.com>
2008-04-22 13:24:51 +0900

    Added lib/iso4217.rb

---
+++ lib/iso4217.rb
@@ -0,0 +1,22 @@
+module ISO4217
+  VERSION = "001"
+
+  class Code
+    attr_reader :code, :num, :locations, :currency, :ccc
+
+    def initialize(table)
+      @code = table[:code]
+      @num = table[:num]
+      @locations = table[:locations]
+      @currency = table[:currency]
+      @ccc = table[:ccc]
+      @obsolete = table[:obsolete]
+    end
+
+    def to_s; @currency; end
+
+    def obsolete; CODE[@obsolete]; end
+  end
+
+  CODE = Hash.new
+end
